af92573bb67fdb815169a195949a6e0787f6e04e
Androsov Sergei <Androsov-07@mail.ru>
2024-03-06 18:39:02 +0300

    Task1

--- Lesson01/Task1/Program.cs
+++ Lesson01/Task1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 100;
+int b = 200;
+int c = 300;
+int d = 400;
+int f = 5;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (f > max)
+{
+    max = f;
+}
+Console.WriteLine(max);
