ce157879cc92cd07b032334af257c463006bf448
Androsov Sergei <Androsov-07@mail.ru>
2024-03-08 20:45:52 +0300

    homework1_task4

---
+++ Lesson01/homrwork_1/4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Вывести все четные числа от 1 до заданного
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+
+while (i <= n)
+{
+    Console.Write(i);
+    Console.Write(' ');
+    i = i + 2;
+}
