884be9c8640c146caf679563094f562c928ba902
Androsov Sergei <Androsov-07@mail.ru>
2024-03-08 21:02:23 +0300

    homework1_task3

---
+++ Lesson01/homrwork_1/3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = number % 2;
+if (i > 0)
+{
+    Console.WriteLine("нечетное");
+}
+else
+{
+    Console.WriteLine("четное");
+}
