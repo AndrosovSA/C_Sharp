c7b2ac6f7e50164752b94884304f983d5a7f0cf5
Androsov Sergei <Androsov-07@mail.ru>
2024-03-08 17:54:39 +0300

    homework1_task1

---
+++ Lesson01/homrwork_1/1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("firstNumber: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("secondNumber: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2)
+{
+    Console.WriteLine("числа равны");
+}
+if (num1 > num2)
+{
+    Console.WriteLine("первое число больше");
+}
+if (num2 > num1)
+{
+    Console.WriteLine("второе число больше");
+}
