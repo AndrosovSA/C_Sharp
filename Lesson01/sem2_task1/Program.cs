60fff9a172a1cbcb4ff0aae60ad80e5cbd1b86cf
Androsov Sergei <Androsov-07@mail.ru>
2024-03-12 16:23:43 +0300

    sem2

---
+++ Lesson01/sem2_task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и удаляет вторую цифру этого числа.
+// Примеры
+// a = 256  => 26
+// a = 891  => 81
+
+Console.Write("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n >= 100 && n <= 999 || n >= -999 && n <= -100)
+{
+    int firstDigit = n / 100; // 256 / 100 = 2.56 =>2 
+    int LastDigit = n % 10;  // 256 % 10 = 250 + 6 =>6
+    int result = firstDigit * 10 + LastDigit;
+    Console.WriteLine("Результат: " + result);
+}
+else Console.WriteLine("Некорректноый ввод");
---
+++ Lesson01/sem2_task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа, которая принимает на вход трехзначное число
+// и возводит вторую цифру этого чисоа в степень, равную третьей цифре.
+
+
+Console.Write("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n >= 100 && n <= 999)
+{
+    int secondDigit = n / 10 % 10;
+    int numDegree = n % 10;
+    int result = 1;
+    int count = 0;
+    while(count < numDegree)
+    {
+        result *= secondDigit; // result = result * secondDigit;
+        count++;
+    }
+    Console.WriteLine(result);
+}
+else Console.WriteLine("Некорректноый ввод");
