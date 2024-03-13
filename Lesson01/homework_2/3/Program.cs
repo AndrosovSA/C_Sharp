ade41d35960d2c8675e4b246f2560a53fb4fc180
Androsov Sergei <Androsov-07@mail.ru>
2024-03-13 09:35:44 +0300

    homework2_task3

---
+++ Lesson01/homework_2/3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Программу, которая принимает на вход целое число из отрезка [10, 99]
+// и показывает наибольшую цифру числа.
+
+Console.Write("Введите число от 10 до 99: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n >= 10 && n <= 99)
+
+{
+int fierstDigit = n / 10;
+int secondDigit = n % 10;
+
+if(fierstDigit > secondDigit)
+{
+    Console.Write("max => " + fierstDigit);
+}
+else
+{
+    Console.Write("max => " + secondDigit);
+}
+}
+else
+{
+    Console.Write("Некорректный ввод");
+}
