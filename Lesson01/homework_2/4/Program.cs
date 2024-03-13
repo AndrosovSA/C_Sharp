3feeb26fd006808747562f7516c1444b633b3889
Androsov Sergei <Androsov-07@mail.ru>
2024-03-13 14:29:07 +0300

    homework2_task4

---
+++ Lesson01/homework_2/4/Program.cs
@@ -0,0 +1,36 @@
+﻿// Программа, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+// Способ 1 (Выводит цифры в обратном порядку)
+
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// while (n>10)
+// {
+// int digit = n % 10;
+// n = n / 10;
+
+// Console.Write(digit);
+// Console.Write(", ");
+// }
+// Console.Write(n);
+
+// Способ 2
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+// определяем разрядность числа
+int numDigit = 1;
+while(n / numDigit >= 10) 
+{
+numDigit = numDigit * 10;
+}
+
+while(numDigit > 0)
+{
+int result = n / numDigit;
+Console.Write(result + ", ");
+n = n - (result * numDigit);
+numDigit = numDigit / 10;
+}
