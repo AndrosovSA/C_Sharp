6ac5a36730bd5a7c64e92dc1efe6ebcb0cb218a5
Androsov Sergei <Androsov-07@mail.ru>
2024-03-14 11:07:06 +0300

    lecture2_4

--- Lesson01/homrwork_1/3/Program.cs
+++ Lesson01/homrwork_1/3/Program.cs
@@ -1,13 +1,27 @@
 ﻿// определить четное или нечетное заданное число
-Console.WriteLine("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-int i = number % 2;
-if (i > 0)
+// int i = number % 2;
+// if (i > 0)
+// {
+//     Console.WriteLine("нечетное");
+// }
+// else
+// {
+//     Console.WriteLine("четное");
+// }
+
+// Программа которая выводит все четные числа в заданном массиве
+
+int[] array = { 4, 15, 134, 1, 1, 600, 56, 456, 4, 35, 33, 34 };
+int i = 0;
+
+while (i < array.Length)
 {
-    Console.WriteLine("нечетное");
-}
-else
-{
-    Console.WriteLine("четное");
+    if (array[i] % 2 == 0)
+    {
+    Console.Write(array[i] + " ");
+    }
+    i = i +1;
 }
