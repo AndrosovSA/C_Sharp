59925a28539fab6576f0fd8df3ba5918aadeff6c
Androsov Sergei <Androsov-07@mail.ru>
2024-03-14 11:04:39 +0300

    lecture2_3

--- Lesson01/Task1/Program.cs
+++ Lesson01/Task1/Program.cs
@@ -1,25 +1,70 @@
-﻿int a = 100;
-int b = 200;
-int c = 300;
-int d = 400;
-int f = 5;
+﻿// Находим самую тяжелую гирю
+// int a = 100;
+// int b = 200;
+// int c = 300;
+// int d = 400;
+// int f = 5;
 
-int max = a;
+// int max = a;
 
-if (b > max)
+// if (b > max)
+// {
+//     max = b;
+// }
+// if (c > max)
+// {
+//     max = c;
+// }
+// if (d > max)
+// {
+//     max = d;
+// }
+// if (f > max)
+// {
+//     max = f;
+// }
+// Console.WriteLine(max);
+
+// Решение задачи с использованием массива
+
+// int[] array = { 100, 200, 300, 400, 5 };
+// int i = 0;
+// int max = array[0];
+
+// while (i < array.Length)
+// {
+//     if (array[i] > max)
+//     {
+//         max = array[i];
+//     }
+//     i = i + 1;
+// }
+// Console.WriteLine(max);
+
+// Цикл for
+
+// int[] array = { 100, 200, 300, 400, 5 };
+// int max = array[0];
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] > max)
+//     {
+//         max = array[i];
+//     }
+// }
+// Console.WriteLine(max);
+
+// Цикл foreach
+
+int[] array = { 100, 200, 300, 400, 5 };
+int max = array[0];
+
+foreach (int e in array)
 {
-    max = b;
-}
-if (c > max)
-{
-    max = c;
-}
-if (d > max)
-{
-    max = d;
-}
-if (f > max)
-{
-    max = f;
+    if (e > max)
+    {
+        max = e;
+    }
 }
 Console.WriteLine(max);
